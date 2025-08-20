9871bae644e43806fdbbe1d66377f024ea74756d
Anh Hoang <97990389+anhhoangt@users.noreply.github.com>
2025-08-20 10:45:16 -0700

    Update Two-Pointer-Algo.py

--- Algorithms/Two-Pointer-Algo.py
+++ Algorithms/Two-Pointer-Algo.py
@@ -93,3 +93,26 @@ def merged_sorted(arr1, arr2):
     return result
 
 print(merged_sorted([1,2,5,6], [4,5,7,8]))
+
+'''
+11. Container With Most Water
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+'''
+def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        left = 0
+        right = len(height) - 1
+
+        while left < right:
+            width = right - left
+            high = min(height[left], height[right])
+            maxArea = max(maxArea, width*high)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
