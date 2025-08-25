c16c8d68117f3a65314cead1ba6f89f17f9c3578
Anh Hoang <97990389+anhhoangt@users.noreply.github.com>
2025-08-25 14:10:19 -0700

    Update Two-Pointer-Algo.py

--- Algorithms/Two-Pointer-Algo.py
+++ Algorithms/Two-Pointer-Algo.py
@@ -153,3 +153,29 @@ You must reverse the order of the words in-place.
                 reverse(start, i - 1)
                 start = i + 1
 
+'''
+3sum:
+'''
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        n =len(nums)
+
+        for i in range(n-2):
+            if i == 0 or (i > 0 and nums[i] != nums[i-1]):
+                left = i+1
+                right = n -1
+                while left < right:
+                    if nums[i] +nums[left]+nums[right]==0:
+                        res.append([nums[i],nums[left],nums[right]])
+                        while left < right and nums[left]==nums[left+1]:
+                            left+=1
+                        while left < right and nums[right]==nums[right-1]:
+                            right-=1
+                        left +=1
+                        right-=1
+                    elif nums[i] +nums[left]+nums[right] >0:
+                        right -=1
+                    else: 
+                        left+=1
+        return res
